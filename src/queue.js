c0116e5111f4079f04db55a1c66caf4e3bd25289
Professional <sania_38@list.ru>
2022-05-01 19:35:38 +0600

    js-ds

--- src/queue.js
+++ src/queue.js
@@ -13,19 +13,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class BinarySearchTree {
   constructor() {
-  }
-  getUnderlyingList() {
-
+    this._root = null;
   }
 
-  enqueue(value) {
-
+  root() {
+    return this._root;
   }
 
-  dequeue() {
+  add(data) {
+    function addWithin(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
 
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
+    }
+
+    this._root  = addWithin(this._root , data);
+  }
+
+  find(data) {
+    function search(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      return data < node.data ? search(node.left, data) : search(node.right, data);
+    }
+
+    return search(this._root , data);
+  }
+
+  remove(data) {
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let maxFromLeft = node.left;
+
+        while (maxFromLeft.right) {
+          maxFromLeft = maxFromLeft.right;
+        }
+        node.data = maxFromLeft.data;
+
+        node.left = removeNode(node.left, maxFromLeft.data);
+
+        return node;
+      }
+    }
+
+    this._root  = removeNode(this._root , data);
   }
 }
 
